705801753fb712fa56035f1d4908d27999e81383
Mai Campbell <jirallc@gmail.com>
2020-06-01 19:43:59 -0400

    feat: completed moves counter timer and reset the game

--- memoryGame.js
+++ memoryGame.js
@@ -1,13 +1,14 @@
 let cards = Array.from(document.querySelectorAll(".memory-cards"));
-let isFlipped = false;
-let lockBoard = false;
+let matchedCard = document.getElementsByClassName("match");
+let timeCounter = document.getElementById("timer")
 let firstGuess, secondGuess;
-let time = 10;
+let time = 100;
 let moves = 0;
 let count = 0;
 let delay = 1200;
 let previousTarget = null;
-// let counter = setInterval(timer, 1000);
+let countdown;
+
 
 //TODO crate start game function that will remove overlay text when game start
 function startGame() {
@@ -15,6 +16,8 @@ function startGame() {
     overlays.forEach((overlay) => {
         overlay.addEventListener("click", function () {
             overlay.classList.remove("visible");
+            resetGame();
+            startCountdown();
         });
     });
 }
@@ -23,12 +26,10 @@ startGame();
 
 // //NOTE: shuffle card
 function shuffleCards() {
-    window.addEventListener("load", (event) => {
         cards.forEach((card) => {
             let shuffle = Math.floor(Math.random() * 17);
             card.style.order = shuffle;
         });
-    });
 }
 cards.forEach((card) => card.addEventListener("click", flipCard));
 shuffleCards();
@@ -36,6 +37,7 @@ shuffleCards();
 //flip card and check if the cards are matching
 
 function flipCard() {
+   
     let clicked = event.target;
 
     if (
@@ -66,11 +68,8 @@ function flipCard() {
         }
         setTimeout(resetGuesses, delay)
     }
-    isFlipped = true
-    // countMove();
-    // timer();
+    victory();
 }
-
 
 //TODO match card and check if card matches
 function checkForMatch() {
@@ -85,8 +84,6 @@ const resetGuesses = () => {
     firstGuess = '';
     secondGuess = '';
     count = 0;
-    time = 100;
-    moves = 0
     isFlipped = false
 
     let selected = document.querySelectorAll(".flip");
@@ -100,13 +97,46 @@ function disableCards() {
     let matched = document.querySelectorAll('.match');
     matched.forEach(card => {
         card.classList.add('disable')
-    })
-   }, 2000);
+    });
+   }, 1000);
+}
+
+//TODO crate Countdown function that ticking down from 100 seconds to 0 if the timer at 0 then display game over overlay
+
+function startCountdown() {
+  countdown = setInterval(() => {
+      time--;
+      if(time == 0 || time <= 0) {
+          document.getElementById('game-over-text').classList.add('visible');
+      }
+      timeCounter.innerHTML = time;
+  }, 1000);
+}
+
+//TODO create count move function to count how many move user clicked on the card and if all card matches before the time run out than display victory overlay text
+
+function victory() {
+    moves++;
+    document.getElementById('flips').innerHTML = moves;
+    if(matchedCard.length === 16) {
+        clearInterval(countdown);
+        document.getElementById('victory-text').classList.add('visible');
+        console.log('all card had matched')
+    }
 }
 
 
-//TODO crate Countdown function that ticking down from 100 seconds to 0 if the timer at 0 then display game over overlay
-// function timer() {
+function resetGame() {
+    firstGuess = '';
+    secondGuess = '';
+    time = 100;
+    timeCounter.innerHTML = time;
+    moves = 0
+    flips.innerHTML = moves;
+    let matched = document.querySelectorAll('.match');
+    matched.forEach(card => {
+        card.classList.remove('match', 'disable')
+        shuffleCards();
+    });
+}
 
-// }
-//TODO create count move function to count how many move user clicked on the card and if all card matches before the time run out than display victory overlay text
